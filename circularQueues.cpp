2d1393a80dcdea6b0fa272c9d63c971f6919dd26
Aswun <aswinlutfian32@gmail.com>
2024-05-30 16:32:10 +0700

    melanjutkan int main 5

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -165,6 +165,13 @@ int main()
                     cout << "Invalid option!!" << endl;
                     break;
                 }
+            }
         }
+        catch (exception& e)
+        {
+            cout << "Check for the values entered." << endl;
+        }
+        
     }
+    return 0;
 }   
